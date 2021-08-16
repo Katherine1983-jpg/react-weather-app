5a51584caabd9f8783a1c52bb7c4d74e4c1af32b
Katherine1983-jpg <79195808+Katherine1983-jpg@users.noreply.github.com>
2021-08-16 11:15:02 +0700

    Removed spinner

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import "./styles.css";
-import Loader from "react-loader-spinner";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <Loader
-        type="ThreeDots"
-        color="black"
-        height={100}
-        width={100}
-        timeout={3000} //3 secs
-      />
     </div>
   );
 }
